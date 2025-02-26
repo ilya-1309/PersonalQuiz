a611ce860ceb6e2e38944fbd0304690a710c7383
Ilya <fekl13in@gmail.com>
2025-02-26 23:55:15 +0300

    creating a structure

---
+++ PersonalQuiz/Models /Question.swift
@@ -0,0 +1,12 @@
+//
+//  Question.swift
+//  PersonalQuiz
+//
+//  Created by Ilya Feklin on 26.02.2025.
+//
+
+struct Question {
+    let title: String
+    let answers: [Answer]
+    let answerCategory: AnswerCategory
+}
